c9feb1da3f21ef706eacf31b98f35f6cd780b459
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-05-27 22:57:46 -0600

    Creacion del proyecto - Login y Main

---
+++ app/src/main/java/com/example/jorgebarraza/mercadomoya_cliente/Actividades/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jorgebarraza.mercadomoya_cliente.Actividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jorgebarraza.mercadomoya_cliente.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
